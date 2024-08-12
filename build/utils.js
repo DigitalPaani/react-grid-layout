9e149ee740469213a9c237e186e94c7d7fa3f282
nalinl-digitalpaani <nalin.luthra@digitalpaani.com>
2024-08-12 23:16:18 +0530

    New build

--- build/utils.js
+++ build/utils.js
@@ -363,7 +363,6 @@ function compactItem(compareWith /*: Layout*/, l /*: LayoutItem*/, compactType /
     // Since we can't grow without bounds horizontally, if we've overflown, let's move it down and try again.
     if (compactV && l.x + l.w > cols) {
       l.x = cols - l.w;
-      l.y++;
       // ALso move element as left as we can
       while (l.x > 0 && !getFirstCollision(compareWith, l)) {
         l.x--;
